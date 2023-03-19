31edd632f7732160082db96349635273b33068c2
Suyash <suyashmishra370@gmail.com>
2023-03-20 05:23:14 +0530

    complete section 18 ,understand redux and  redux/toolkit usage in code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Counter from './components/Counter';
-
+import { useSelector } from "react-redux";
+import React from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const isLoggedIn = !isAuthenticated ? <Auth /> : <UserProfile />;
+
   return (
-    <Counter />
+    <React.Fragment>
+      <Header />
+      {isLoggedIn}
+      <Counter />
+    </React.Fragment>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,38 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../store/auth";
 
 const Header = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const logoutButtonHandler = () => {
+    dispatch(authActions.logout());
+  };
+  const loginButtonHandler = () => {
+    dispatch(authActions.login());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {isAuthenticated && (
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutButtonHandler}>Logout</button>
+            </li>
+          </ul>
+        )}
+        {!isAuthenticated && (
+          <ul>
+            <button onClick={loginButtonHandler}>Login</button>
+          </ul>
+        )}
       </nav>
     </header>
   );
