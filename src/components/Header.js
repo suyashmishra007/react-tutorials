0a6daa41d7250546687c1233f5d916cc83bd62aa
Suyash <suyashmishra370@gmail.com>
2023-03-22 13:56:01 +0530

    Refactor isAuth conditions

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,29 +11,27 @@ const Header = () => {
   const loginButtonHandler = () => {
     dispatch(authActions.login());
   };
+  const isUserAuthenticated = isAuthenticated ? (
+    <ul>
+      <li>
+        <a href="/">My Products</a>
+      </li>
+      <li>
+        <a href="/">My Sales</a>
+      </li>
+      <li>
+        <button onClick={logoutButtonHandler}>Logout</button>
+      </li>
+    </ul>
+  ) : (
+    <ul>
+      <button onClick={loginButtonHandler}>Login</button>
+    </ul>
+  );
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        {isAuthenticated && (
-          <ul>
-            <li>
-              <a href="/">My Products</a>
-            </li>
-            <li>
-              <a href="/">My Sales</a>
-            </li>
-            <li>
-              <button onClick={logoutButtonHandler}>Logout</button>
-            </li>
-          </ul>
-        )}
-        {!isAuthenticated && (
-          <ul>
-            <button onClick={loginButtonHandler}>Login</button>
-          </ul>
-        )}
-      </nav>
+      <nav>{isUserAuthenticated}</nav>
     </header>
   );
 };
