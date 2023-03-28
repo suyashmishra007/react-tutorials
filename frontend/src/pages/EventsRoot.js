9a25bffb7889584cebf65dbcbb896c71aa75ab3b
Suyash <suyashmishra370@gmail.com>
2023-03-28 09:08:24 +0530

    completed till 283

---
+++ frontend/src/pages/EventsRoot.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import EventsNavigation from "../components/EventsNavigation";
+
+const EventsRootLayout = () => {
+  return (
+    <>
+      <EventsNavigation />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default EventsRootLayout;
