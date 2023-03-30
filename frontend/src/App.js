201540adc56e352440dfbb5f19016049bd7c7fbf
Suyash <suyashmishra370@gmail.com>
2023-03-30 14:01:26 +0530

    completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import EditEventPage from "./pages/EditEvent";
 import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from "./pages/EventDetail";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
               { path: "edit", element: <EditEventPage /> },
             ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,20 +1,20 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
+
 import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const data = useLoaderData();
-  return (
-    <>
-      <EventItem event={data.event} />
-    </>
-  );
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
 
 export async function loader({ request, params }) {
   const id = params.eventId;
+
   const response = await fetch("http://localhost:8080/events/" + id);
+
   if (!response.ok) {
     throw json(
       { message: "Could not fetch details for selected event." },
@@ -25,4 +25,20 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({ request, params }) {
+  const eventId = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+  if (!response.ok) {
+    throw json(
+      { message: "Could not delete event." },
+      {
+        status: 500,
+      }
+    );
+  }
+  return redirect("/events");
 }
